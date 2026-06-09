0a78e99e3346f047717b65746926c6b55f387371
Barnabas <davidbarnabas666@gmail.com>
2026-06-09 20:46:28 +0100

    Remove unused Hero and Features components from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import Features from "./components/Features/Features";
-import Hero from "./components/Hero/Hero";
+
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Home />
-      <Hero />
-      <Features />
     </>
   );
 }
